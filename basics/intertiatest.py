06401cb172c4c4d8b8acfd16fb8385d7b3a5351a
ldj789 <ljacobs.3a@gmail.com>
2019-12-07 17:51:17 +0300

    reorg

--- basics/intertiatest.py
+++
@@ -1,9 +0,0 @@
-import time
-
-import di_sensors
-import easygopigo3 as easy
-# from di_sensors.inertial_measurement_unit import InertialMeasurementUnit
-
-
-# /usr/local/lib/python3.5/dist-packages/DI_Sensors-1.0.0-py3.5.egg/di_sensors/
-gpg = easy.EasyGoPiGo3()
