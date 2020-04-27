590fcbab5aa4970f1ed2426ffa6e7ef67b450c73
ldj789 <ljacobs.3a@gmail.com>
2020-04-26 22:11:14 -0500

    more routes for demo paths

--- drive/routes.py
+++ drive/routes.py
@@ -5,7 +5,7 @@ gpg = EasyGoPiGo3()
 
 
 def drive_inst_1():
-    """Drive Instructions 1
+    """Drive Instructions 1 (for demo path 1)
 
     Go 50cm, right turn, 50cm, left turn, 50cm
     :return: None
@@ -33,3 +33,32 @@ def drive_inst_2():
         gpg.turn_degrees(90)
     gpg.stop()
 
+
+def drive_inst_3():
+    """Drive Instructions for demo path 2
+
+    Go 125cm, left 90 degrees, 50cm, right 90 degrees, 125cm
+    :return: None
+    """
+    pro_name = multiprocessing.current_process().name
+    print("Starting Drive Process {}".format(pro_name))
+    gpg.drive_cm(125)
+    gpg.turn_degrees(-90)
+    gpg.drive_cm(50)
+    gpg.turn_degrees(-90)
+    gpg.drive_cm(125)
+    gpg.stop()
+
+
+def drive_inst_mini():
+    """Drive Instructions 2
+
+    Drive in a square with side 75cm
+    :return: None
+    """
+    pro_name = multiprocessing.current_process().name
+    print("Starting Drive Process {}".format(pro_name))
+    for _ in range(4):
+        gpg.drive_cm(10)
+        gpg.turn_degrees(45)
+    gpg.stop()
--- measure/measure_return_3n.py
+++ measure/measure_return_3n.py
@@ -9,8 +9,8 @@ from easygopigo3 import EasyGoPiGo3
 from di_sensors.inertial_measurement_unit import InertialMeasurementUnit
 from drive.utils import print_reading, get_reading
 from drive.control import drive_home, return_to_point
-from drive.routes import drive_inst_1, drive_inst_2
-### imports for plotting
+from drive.routes import drive_inst_1, drive_inst_2, drive_inst_3, drive_inst_mini
+# ## imports for plotting
 import math as math
 import matplotlib.pyplot as plt
 import pandas as pd 
@@ -18,10 +18,11 @@ import numpy as np
 
 
 # Setup Manual Inputs (HARD CODES)
-test_drive_instr = drive_inst_1
+test_drive_instr = drive_inst_mini
 attempt_return = False
 saving_data = False
-draw_path=True
+draw_path = True
+init_x, init_y = 0, 0  # For test path 2 use 250, 50
 
 # Setup Sensors
 imu = InertialMeasurementUnit(bus="GPG3_AD1")
@@ -41,74 +42,67 @@ def get_position(right_prev,left_prev,euler_x_prev,theta_prev):
     euler_x=euler[0]
 
     encoder = gpg.read_encoders()
-    left_enc=encoder[0]
-    right_enc= encoder[1]
-    lr_delta=left_enc-right_enc
-    y_delta=left_enc-left_prev
-    x_delta=right_enc-right_prev
-    lr_avg=(x_delta+y_delta)/2
+    left_enc = encoder[0]
+    right_enc = encoder[1]
+    lr_delta = left_enc-right_enc
+    y_delta = left_enc-left_prev
+    x_delta = right_enc-right_prev
+    lr_avg = (x_delta+y_delta)/2
     if(abs(lr_delta)>2):
         
         theta_delta=euler_x-euler_x_prev
     else:
         theta_delta=0
-    theta=theta_prev+theta_delta
-    x=math.sin(theta*0.0174533)*lr_avg
-    y=math.cos(theta*0.0174533)*lr_avg
+    theta = theta_prev+theta_delta
+    x = math.sin(theta*0.0174533) * lr_avg
+    y = math.cos(theta*0.0174533) * lr_avg
     read_distance = 0
     res = {
-
-
         "x": x,
         "y": y
-
-        
     }
-    ##print(res)
-    return left_enc,right_enc,x,y,res,euler_x,theta
+    # #print(res)
+    return left_enc, right_enc, x, y, res, euler_x, theta
 
 
 # Initialize Measurements for drive
-i=0
+i = 0
 t1 = datetime.now()
 data = []
-right_prev=0
-left_prev=0
-x_total=0
-y_total=0
+right_prev = 0
+left_prev = 0
+x_total = init_x
+y_total = init_y
 euler = imu.read_euler()
-euler_x_prev=euler[0]
-theta_prev=0
+euler_x_prev = euler[0]
+theta_prev = 0
 
 # Start Driving
 drive_process.start()
 
 # Measure while driving loop
-while  i<100:
+while i < 100:
     # Execute
-    ##print_reading()
-    #data.append(get_reading())
+    # #print_reading()
+    # data.append(get_reading())
     
     t2 = datetime.now()
-    left_enc,right_enc,x,y,res,euler_x,theta=get_position(right_prev,left_prev,euler_x_prev,theta_prev)
-    euler_x_prev=euler_x
-    theta_prev=theta
-    right_prev=right_enc
-    left_prev=left_enc
-    x_total=x_total+x
-    y_total=y_total+y
-    ##print("x= %2.2f, y=%2.2f, dx= %2.2f, dy=%2.2f, euler==%2.2f  theta==%2.2f " % (x_total/44, y_total/44,x/44, y/44, euler_x,theta))
-    print("x= %2.2f, y=%2.2f,  euler==%2.2f  theta==%2.2f " % (x_total/44, y_total/44, euler_x,theta))
-    ##print(imu.read_euler()[0])
-    ##print("Duration: {}".format(t2 - t1))
+    left_enc, right_enc, x, y, res, euler_x, theta = get_position(right_prev, left_prev, euler_x_prev, theta_prev)
+    euler_x_prev = euler_x
+    theta_prev = theta
+    right_prev = right_enc
+    left_prev = left_enc
+    x_total = x_total+x
+    y_total = y_total+y
+    # #print("x= %2.2f, y=%2.2f, dx= %2.2f, dy=%2.2f, euler==%2.2f  theta==%2.2f " % (x_total/44, y_total/44,x/44, y/44, euler_x,theta))
+    print("x= %2.2f, y=%2.2f,  euler==%2.2f  theta==%2.2f " % (x_total/44, y_total/44, euler_x, theta))
+    # #print(imu.read_euler()[0])
+    # #print("Duration: {}".format(t2 - t1))
     # print(timedelta(t2, t1))
     res2 = {
-
-        "t":str(t2),
-        "x": x_total  ,
+        "t": str(t2),
+        "x": x_total,
         "y": y_total
-
-        
     }
     data.append(res2)
     
@@ -117,16 +111,17 @@ while  i<100:
     t1 = t2
     time.sleep(.1)
 
-print (data[1])
+print(data[1])
     
 
-print ("printing path")
+print("printing path")
 plt.style.use('seaborn-whitegrid')
 df = pd.DataFrame(data) 
-#'lr_delta': -1, 'euler_x': 240.4375, 'left_enc': 2585, 'y': 4.216956575971679, 'theta': -32.5, 'x_delta': 4, 'right_enc': 2586, 'x': -2.6864990668841138, 'y_delta': 6
-##print(df[1])
+# 'lr_delta': -1, 'euler_x': 240.4375, 'left_enc': 2585, 'y': 4.216956575971679, 'theta': -32.5, 'x_delta': 4,
+#  'right_enc': 2586, 'x': -2.6864990668841138, 'y_delta': 6
+# print(df[1])
 print(df.head())
-##df.columns = ['lr_delta','euler_x',"left_enc","y",'theta','x_delta','right_enc','x','y_delta']
+# df.columns = ['lr_delta','euler_x',"left_enc","y",'theta','x_delta','right_enc','x','y_delta']
 fig = plt.figure()
 plt.plot(df.x, df.y, 'o', color='black')
 fig.savefig('plot.png')
@@ -134,26 +129,24 @@ df.to_csv("df.csv", index=False)
 
 
 if attempt_return:
-    ##print(imu.read_euler()[0]) 
-    distance_back=math.sqrt(x_total*x_total+y_total*y_total)
+    # #print(imu.read_euler()[0])
+    distance_back = math.sqrt(x_total*x_total+y_total*y_total)
 
-    direction_back=90-theta+90+90
+    direction_back = 90 - theta + 90 + 90
 
     print("Back direction= %8.2f dist=%8.2f" % (direction_back, distance_back/44))
 
-
     gpg.turn_degrees(direction_back)
-    print("back inc= %8.2f back cm=%8.2f" % (distance_back/44, distance_back/44*2.54))
-    ###gpg.drive_cm(distance_back/44*2.54)  
+    print("back inc= %8.2f back cm=%8.2f" % (distance_back / 44, distance_back / 44 * 2.54))
+    # ##gpg.drive_cm(distance_back/44*2.54)
     gpg.stop()  
     # Save Out
-    #with open('data.pkl', 'wb') as f:
-        #pickle.dump(data, f)
+    # with open('data.pkl', 'wb') as f:
+        # pickle.dump(data, f)
 
 if saving_data:
     # Save Out
     with open('data.pkl', 'wb') as f:
         pickle.dump(data, f)
 
-
-#### run file python3 -m measure.measure_return_3
+# run file python3 -m measure.measure_return_3
