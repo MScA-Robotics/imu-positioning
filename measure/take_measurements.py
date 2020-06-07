c633084c06525b0f230504e1472e65befd44d6c2
Drew Jacobs <ljacobs.3a@gmail.com>
2020-06-07 18:02:54 +0300

    update take_measurements

--- measure/take_measurements.py
+++ measure/take_measurements.py
@@ -18,8 +18,9 @@ from drive.utils import get_reading
 import drive.routes as routes
 
 # Setup Manual Inputs (HARD CODES)
-test_drive_instr = routes.turn_series
+test_drive_instr = routes.drive_demo_1
 drive_name = 'sample_drive_3'
+saving_data = False
 write_header = True
 file_out = 'turns_5_30.csv'
 
@@ -29,10 +30,8 @@ gpg = EasyGoPiGo3()
 gpg.reset_encoders()
 atexit.register(gpg.stop)
 
-# Setup File
-q = multiprocessing.Queue()
-
 # Setup Standard Drive
+q = multiprocessing.Queue()
 drive_process = multiprocessing.Process(
     name='drive',
     target=test_drive_instr,
@@ -48,7 +47,8 @@ while i < 15:
     reading['drive name'] = drive_name
     data.append(reading)
     i += 1
-    print(q.get())
+    if not q.empty():
+        print(q.get())
     time.sleep(2)
 
 # Wrap up processes, print and save
@@ -56,17 +56,18 @@ drive_process.join()
 pprint(data)
 print('done')
 
-output_file_name = os.path.join(os.getcwd(), 'offline', 'data', file_out)
-# keys = data[0].keys()
-keys = ['drive name', 'time', 'left_enc', 'right_enc',
-        'euler_x', 'euler_y', 'euler_z',
-        'mag_x', 'mag_y', 'mag_z',
-        'accel_x', 'accel_y', 'accel_z',
-        'gyro_x', 'gyro_y', 'gyro_z',
-        ]
-    
-with open(output_file_name, 'a') as f:
-    dict_writer = csv.DictWriter(f, keys)
-    if write_header:
-        dict_writer.writeheader()
-    dict_writer.writerows(data)
+if saving_data:
+    output_file_name = os.path.join(os.getcwd(), 'offline', 'data', file_out)
+    # keys = data[0].keys()
+    keys = ['drive name', 'time', 'left_enc', 'right_enc',
+            'euler_x', 'euler_y', 'euler_z',
+            'mag_x', 'mag_y', 'mag_z',
+            'accel_x', 'accel_y', 'accel_z',
+            'gyro_x', 'gyro_y', 'gyro_z',
+            ]
+
+    with open(output_file_name, 'a') as f:
+        dict_writer = csv.DictWriter(f, keys)
+        if write_header:
+            dict_writer.writeheader()
+        dict_writer.writerows(data)
